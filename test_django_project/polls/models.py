d9bdd7f5519b9774fbc51d78191af6984ebe85d4
Artyom Lukianov <cynepco3hahue@gmail.com>
2014-10-26 01:19:33 +0200

    Before Part 6

--- test_django_project/polls/models.py
+++ test_django_project/polls/models.py
@@ -9,7 +9,9 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
@@ -25,3 +27,4 @@ class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
